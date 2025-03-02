8f657fcdcb0e5df3ec7d1aced3be647298116e9c
fatousy15 <fatouchaaasy15@gmail.com>
2025-03-02 23:43:40 +0100

    first commit

---
+++ Marque.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2> ASUS</h2>
+    <h2> DELL</h2>
+    <h2> MSI</h2>
+    <h2> LENOVO</h2>
+    <h2> LEGION </h2>
+    <h2> MICROSOFT</h2>
+    <h2> SONY</h2>
+    <h2> SAMSUNG</h2>
+</body>
+</html>
